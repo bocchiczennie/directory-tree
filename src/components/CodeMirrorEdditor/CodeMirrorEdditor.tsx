d2e91e5bb65ee3ad7954020199423326dbf41ee8
bocchiczennie <bocchiczennie@gmail.com>
2025-03-04 18:43:36 +0900

    first commit

---
+++ src/components/CodeMirrorEdditor/CodeMirrorEdditor.tsx
@@ -0,0 +1,3 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { EdditorView } from "codemirror";
+import type { FC } from "react";
---
+++ src/components/FormattedTextDisplayArea/FormattedTextDisplayArea.tsx
@@ -0,0 +1,7 @@
+export const FormattedTextDisplayArea = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
